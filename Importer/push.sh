6490bd1e04569a47174ec8293d892ab7b581dbe5
kuroko <formalingum@gmail.com>
2020-07-01 09:55:26 +0900

    コミット前に古いマニュアルzipと比較し、変更がなければ処理を終了する機能を追加

--- Importer/push.sh
+++ Importer/push.sh
@@ -2,12 +2,26 @@
 
 time=$(TZ=UTC-9 date '+%F %R')
 
-rm -rf ./Converted
-rm -rf ./repo
-rm -rf ./tmp
-rm -rf ./GMS2_Japanese-master/
-git pull origin master
-git add -A
-git status
-git commit -m "Update from ParaTranz: ${time}"
-git push
+ZIP_PATH='Converted/GMS2_Japanese-master.zip'
+NO_CHANGES=0
+
+if [ -e $ZIP_PATH ]; then
+  cp Converted/GMS2_Japanese-master.zip ./ -a -f
+else
+  NO_CHANGES=1
+fi
+
+rm -rf Converted
+rm -rf repo
+rm -rf tmp
+rm -rf GMS2_Japanese-master
+
+if [ $NO_CHANGES -eq 0 ]; then
+  git pull origin master
+  git add -A
+  git status
+  git commit -m "Update from ParaTranz: ${time}"
+  git push
+else
+  echo 'No changes were detected. Processing is complete.'
+fi
--- Importer/zipper.py
+++ Importer/zipper.py
@@ -1,4 +1,35 @@
 import shutil
+import filecmp
+import os
+import zipfile
+
+zip_root = './GMS2_Japanese-master'
+new_zip = './Converted/GMS2_Japanese-master'
+old_zip = './GMS2_Japanese-master.zip'
+
 
 if __name__ == '__main__':
-    shutil.make_archive('./GMS2_Japanese-master', 'zip', root_dir='./GMS2_Japanese-master')
+    shutil.make_archive(new_zip, 'zip', root_dir=zip_root)
+
+    new_zip = new_zip + '.zip'
+
+    l_old = []
+    l_new = []
+
+    with zipfile.ZipFile(old_zip) as zip_file:
+        infos = zip_file.infolist()
+
+        for info in infos:
+            l_old.append(zip_file.read(info.filename))
+
+
+    with zipfile.ZipFile(new_zip) as zip_file:
+        infos = zip_file.infolist()
+
+        for info in infos:
+            l_new.append(zip_file.read(info.filename))
+
+
+    if set(l_old) == set(l_new):  
+        print('This is the same zip file as the old one. No changes were detected.')
+        os.remove(new_zip)
